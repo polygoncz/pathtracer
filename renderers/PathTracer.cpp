5dab1c1eeaedefe65f44fd846f01cdded516df4c
Pavel Lokvenc <polygoncz@gmail.com>
2015-11-11 12:25:27 +0100

    Add PathTracer.cpp/.h

---
+++ renderers/PathTracer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by pavel on 11.11.15.
+//
+
+#include "PathTracer.h"
---
+++ renderers/PathTracer.h
@@ -0,0 +1,15 @@
+//
+// Created by pavel on 11.11.15.
+//
+
+#ifndef PATHTRACER_PATHTRACER_H
+#define PATHTRACER_PATHTRACER_H
+
+
+class PathTracer
+{
+
+};
+
+
+#endif //PATHTRACER_PATHTRACER_H
